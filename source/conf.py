afb14c1a4757cf088f758ccee0650030a09b2476
martinqt <m.ki2@laposte.net>
2013-01-11 21:58:44 +0100

    Set copyrights

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GitExtensionsDoc'
-copyright = u'2013, Contributors'
+copyright = u'2013, GitExtensions'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
