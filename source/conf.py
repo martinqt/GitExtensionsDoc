6868f2b4a39fc894c44711c8903407da596acbf5
Gerhard Olsson <gerhard.nospam@gmail.com>
2018-01-29 00:29:37 +0100

    Update copyright to 2018

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GitExtensions'
-copyright = u'2008-2017, GitExtensions'
+copyright = u'2008-2018, GitExtensions'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
